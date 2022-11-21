ae6832c06e4cab916a1d2afa84e56a5907f080f7
ibrahimsamson <todder1998@yahoo.com>
2022-11-21 14:56:25 +0800

    changed the gausian field behaviour

---
+++ demo.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from numpy.fft import fftn, ifftn, fftshift, ifftshift
+from skimage import io, color, transform
+
+from phase_retrival1 import get_image_from_url, plot 
+
+def retrieve():
+    pass
+
+retrieve()
